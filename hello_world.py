ccdb70aae0e7f75c99bc5592549bb759a219991c
Dror Atariah <d.atariah@rebuy.com>
2017-09-26 17:56:27 +0200

    Show how to turn subcommand into standalone

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,8 @@ import click
 @click.command()
 def hello_world():
     click.echo("Hello world!")
+
+# By adding the following, this command can be executed
+# as a standalone CLI application
+if __name__ == "__main__":
+    hello_world()
